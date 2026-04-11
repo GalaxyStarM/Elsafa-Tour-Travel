9f2e9de2bdd5516ab2404041be611885c123d8bb
Tri Murniati <tri.murniati2735@student.unri.ac.id>
2026-04-12 01:18:16 +0700

    add routes for mitra and paket

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MitraController;
+use App\Http\Controllers\PaketController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// --- Tambahkan route ini di dalam group middleware ---
+// Route::middleware(['auth', 'admin'])->group(function () {
+
+    // Data Mitra
+    Route::get('/mitras',           [MitraController::class, 'index'])->name('mitras.index');
+    Route::post('/mitras',          [MitraController::class, 'store'])->name('mitras.store');
+    Route::get('/mitras/{mitra}',   [MitraController::class, 'show'])->name('mitras.show');
+    Route::put('/mitras/{mitra}',   [MitraController::class, 'update'])->name('mitras.update');
+    Route::delete('/mitras/{mitra}',[MitraController::class, 'destroy'])->name('mitras.destroy');
+
+    // Paket
+    Route::get('/pakets',           [PaketController::class, 'index'])->name('pakets.index');
+    Route::post('/pakets',          [PaketController::class, 'store'])->name('pakets.store');
+    Route::put('/pakets/{paket}',   [PaketController::class, 'update'])->name('pakets.update');
+    Route::delete('/pakets/{paket}',[PaketController::class, 'destroy'])->name('pakets.destroy');
+
+// });
